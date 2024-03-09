03985ca13e5710ef6c3f8c3600bba49ef6c64df3
Isabel <isabelroldancordoba@hotmail.com>
2024-03-09 22:42:54 +0100

    IRC Finalizado

--- src/main/java/psp/sockets/Servidor/Main.java
+++ src/main/java/psp/sockets/Servidor/Main.java
@@ -146,6 +146,7 @@ public class Main {
                 case 4:
                     // Opción para salir
                     System.out.println("Cerrando sesión...");
+                    System.exit(0);
 
                     // Limpiar el flujo de entrada
                     try {
@@ -167,7 +168,7 @@ public class Main {
      * @param outputStream  El flujo de salida para enviar datos al servidor.
      * @param inputStream   El flujo de entrada para recibir datos del servidor.
      */
-    private static void mostrarMenuOperario(ObjectOutputStream outputStream, ObjectInputStream inputStream) throws IOException {
+    private static void mostrarMenuOperario(ObjectOutputStream outputStream, ObjectInputStream inputStream) throws IOException, ClassNotFoundException {
         Scanner scanner = new Scanner(System.in);
         int opcion;
         do {
@@ -180,227 +181,121 @@ public class Main {
             System.out.println("6. Salir");
             System.out.println("Seleccione una opción:");
 
-            outputStream.writeInt(opcion = scanner.nextInt());
+            //outputStream.writeInt(opcion = scanner.nextInt());
+            opcion = scanner.nextInt();
 
             switch (opcion) {
                 case 1:
+                    outputStream.writeObject(1);
                     // Lógica para ingresar un nuevo usuario en el banco
                     System.out.println("Opción seleccionada: Ingresar un nuevo usuario en el banco");
 
                     // Solicitar al operario que ingrese los datos del nuevo usuario
                     System.out.println("Ingrese el nombre del nuevo usuario:");
-                    String nuevoUsuarioNombre = scanner.nextLine();
+                    String nuevoUsuarioNombre = scanner.next(); // Lee solo una palabra
+                    scanner.nextLine();
+                    outputStream.writeObject(nuevoUsuarioNombre);
+
                     System.out.println("Ingrese la contraseña del nuevo usuario:");
                     String nuevoUsuarioContraseña = scanner.nextLine();
+                    outputStream.writeObject(nuevoUsuarioContraseña);
+
                     System.out.println("Ingrese el rol del nuevo usuario:");
                     String nuevoUsuarioRol = scanner.nextLine();
+                    outputStream.writeObject(nuevoUsuarioRol);
 
-                    // Crear un nuevo objeto de tipo User con los datos ingresados
-                    User nuevoUsuario = new User();
-                    nuevoUsuario.setName(nuevoUsuarioNombre);
-                    nuevoUsuario.setPassword(nuevoUsuarioContraseña);
-                    nuevoUsuario.setRole(nuevoUsuarioRol);
+                    String UsuarioNuevo = (String) inputStream.readObject();
+                    System.out.println(UsuarioNuevo);
 
-                    try {
-                        // Enviar el nuevo usuario al servidor para ser ingresado en el banco
-                        outputStream.writeInt(opcion);
-                        outputStream.writeObject(nuevoUsuario);
-                        outputStream.flush();
-
-                        // Recibir la confirmación del servidor
-                        String respuestaServidor = inputStream.readUTF();
-                        System.out.println(respuestaServidor);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                     break;
 
                 case 2:
+                    outputStream.writeObject(2);
                     // Lógica para crear una nueva cuenta bancaria
                     System.out.println("Opción seleccionada: Crear una nueva cuenta bancaria");
 
                     // Solicitar al operario que ingrese el número de cuenta
                     System.out.println("Ingrese el número de la nueva cuenta:");
                     int nuevoNumeroCuenta = scanner.nextInt();
+                    outputStream.writeObject(nuevoNumeroCuenta);
+
+                    // Solicitar al operario que el balance con el que se va a iniciar la cuenta
+                    System.out.println("Ingrese el balance con el que se va a iniciar la cuenta:");
+                    float balance = scanner.nextInt();
+                    outputStream.writeObject(balance);
 
                     // Solicitar al operario que ingrese el nombre del usuario para asociar la cuenta
                     System.out.println("Ingrese el nombre del usuario asociado a la cuenta:");
-                    String nombreUsuario = scanner.nextLine();
+                    String nombreUsuario = scanner.next();
+                    scanner.nextLine();
+                    outputStream.writeObject(nombreUsuario);
 
-                    // Solicitar al servidor que busque al usuario en la base de datos
-                    try {
-                        outputStream.writeUTF(nombreUsuario);
-                        outputStream.flush();
+                    String CuentaNueva = (String) inputStream.readObject();
+                    System.out.println(CuentaNueva);
 
-                        // Recibir la respuesta del servidor
-                        String respuestaServidor = inputStream.readUTF();
 
-                        if (respuestaServidor.equals("Usuario encontrado")) {
-                            // Si el usuario está en la base de datos, continuar con la creación de la cuenta
-
-                            // Crear un nuevo objeto de tipo User con el nombre del usuario proporcionado
-                            User usuarioAsociado = new User();
-                            usuarioAsociado.setName(nombreUsuario);
-
-                            // Crear un nuevo objeto de tipo Account con los datos ingresados
-                            Account nuevaCuenta = new Account();
-                            nuevaCuenta.setAcountNumber(nuevoNumeroCuenta);
-                            nuevaCuenta.setBalance(0); // Establecer el saldo inicial en 0
-                            nuevaCuenta.setUser(usuarioAsociado); // Establecer el usuario asociado a la cuenta
-
-                            // Enviar la nueva cuenta al servidor para ser creada
-                            outputStream.writeObject(nuevaCuenta);
-                            outputStream.flush();
-
-                            // Recibir la confirmación del servidor
-                            String respuestaCreacionCuenta = inputStream.readUTF();
-                            System.out.println(respuestaCreacionCuenta);
-                        } else {
-                            // Si el usuario no se encuentra en la base de datos, mostrar un mensaje y volver al menú
-                            System.out.println("El usuario proporcionado no se encuentra en la base de datos.");
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                     break;
 
 
                 case 3:
+                    outputStream.writeObject(3);
                     // Lógica para ver los datos de una cuenta bancaria
                     System.out.println("Opción seleccionada: Ver los datos de una cuenta bancaria");
 
                     // Solicitar al operario que ingrese el número de cuenta
                     System.out.println("Ingrese el número de cuenta:");
                     int accountNumber = scanner.nextInt();
+                    outputStream.writeObject(accountNumber);
 
-                    try {
-                        // Enviar el número de cuenta al servidor
-                        outputStream.writeInt(accountNumber);
-                        outputStream.flush();
+                    System.out.println("Ingrese el usuario:");
+                    String userAccount = scanner.next();
+                    outputStream.writeObject(userAccount);
 
-                        // Recibir la cuenta del servidor
-                        Account cuenta = (Account) inputStream.readObject();
+                    String datosCuenta = (String) inputStream.readObject();
+                    System.out.println(datosCuenta);
 
-                        // Verificar si se encontró la cuenta
-                        if (cuenta != null) {
-                            // Mostrar los detalles de la cuenta
-                            System.out.println("Datos de la cuenta:");
-                            System.out.println("ID de cuenta: " + cuenta.getId());
-                            System.out.println("Saldo: " + cuenta.getBalance());
-                            // Puedes mostrar más detalles de la cuenta si lo deseas
-                        } else {
-                            System.out.println("La cuenta con el número proporcionado no fue encontrada.");
-                        }
-                    } catch (IOException | ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
                     break;
 
                 case 4:
+                    outputStream.writeObject(4);
                     // Lógica para ver los datos de un cliente
                     System.out.println("Opción seleccionada: Ver los datos de un cliente");
 
-                    // Solicitar al operario que ingrese el nombre de usuario del cliente
-                    System.out.println("Ingrese el nombre de usuario del cliente:");
-                    String nombreCliente = scanner.nextLine();
+                    System.out.println("Ingrese el usuario:");
+                    String userShow = scanner.next();
+                    outputStream.writeObject(userShow);
 
-                    try {
-                        // Enviar el nombre de usuario al servidor
-                        outputStream.writeUTF(nombreCliente);
-                        outputStream.flush();
+                    String datosUsuario = (String) inputStream.readObject();
+                    System.out.println(datosUsuario);
 
-                        // Recibir los datos del cliente desde el servidor
-                        User cliente = (User) inputStream.readObject();
 
-                        // Verificar si se encontró el cliente
-                        if (cliente != null) {
-                            // Mostrar los detalles del cliente
-                            System.out.println("Datos del cliente:");
-                            System.out.println("Nombre de usuario: " + cliente.getName());
-                            // Mostrar más detalles del cliente si es necesario
-                        } else {
-                            System.out.println("El cliente con el nombre de usuario proporcionado no fue encontrado.");
-                        }
-                    } catch (IOException | ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
                     break;
 
                 case 5:
+                    outputStream.writeObject(5);
                     // Lógica para eliminar una cuenta bancaria
                     System.out.println("Opción seleccionada: Eliminar una cuenta bancaria");
 
                     // Solicitar al operario que ingrese el número de cuenta a eliminar
                     System.out.println("Ingrese el número de cuenta a eliminar:");
                     int accountNumberToDelete = scanner.nextInt();
+                    outputStream.writeObject(accountNumberToDelete);
 
-                    try {
-                        // Enviar el número de cuenta al servidor para eliminar la cuenta
-                        outputStream.writeInt(accountNumberToDelete);
-                        outputStream.flush();
+                    System.out.println("Ingrese el usuario:");
+                    String userAccountDelete = scanner.next();
+                    outputStream.writeObject(userAccountDelete);
 
-                        // Recibir la confirmación del servidor
-                        String respuestaServidor = inputStream.readUTF();
-                        System.out.println(respuestaServidor);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    String mensajeEliminar = (String) inputStream.readObject();
+                    System.out.println(mensajeEliminar);
+
                     break;
 
                 case 6:
+                    outputStream.writeObject(6);
                     // Opción para salir
                     System.out.println("Cerrando sesión...");
+                    System.exit(0);
 
-                    // Limpiar el flujo de entrada
-                    try {
-                        inputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    // Volver al inicio de sesión
-                    System.out.println("Ingrese su nombre de usuario:");
-                    String nombreUsuarioo = scanner.nextLine();
-                    System.out.println("Ingrese su contraseña:");
-                    String contraseña = scanner.nextLine();
-
-                    // Hash de la contraseña
-                    String hashContraseña = String.valueOf(hash(contraseña));
-
-                    // Crear objeto de credenciales
-                    Credentials credentials = new Credentials(nombreUsuarioo, hashContraseña);
-
-                    // Enviar credenciales al servidor
-                    try {
-                        outputStream.writeObject(credentials);
-
-                        // Recibir respuesta del servidor
-                        String respuesta = inputStream.readUTF();
-                        System.out.println(respuesta);
-
-                        // Si la respuesta indica un inicio de sesión exitoso, mostrar el menú correspondiente
-                        if (respuesta.equals("Inicio de sesión exitoso")) {
-                            // Leer el rol del usuario del servidor
-                            String rol = inputStream.readUTF();
-
-                            // Obtener el usuario del servidor
-                            //usuario = (User) inputStream.readObject();
-
-                            // Mostrar el menú correspondiente según el rol
-                            switch (rol) {
-                                case "Cajero":
-                                    //mostrarMenuCajero(outputStream, inputStream);
-                                    break;
-                                case "Operario":
-                                    mostrarMenuOperario(outputStream, inputStream);
-                                    break;
-                                default:
-                                    System.out.println("Error: Rol desconocido");
-                            }
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                     break;
                 default:
                     System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
